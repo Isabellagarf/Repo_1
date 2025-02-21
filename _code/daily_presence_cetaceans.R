4fd3e97a1981d72c3a399c3da50d51e1971e3dc6
Isabella Garfield <isabellagarfield@gmail.com>
2025-02-21 13:40:10 -0500

    Update daily_presence_cetaceans.R

--- _code/daily_presence_cetaceans.R
+++ _code/daily_presence_cetaceans.R
@@ -2,7 +2,7 @@
 # based on code from: GE Davis 02/10/2023, adapted from Davis et al 2020 5 species presence plot
 
 
-#### LOAD REQUIRED PACKAGES ####
+#### LOAD REQUIRED PACKAGES #####
 
 library(tidyverse)
 library(lubridate)
